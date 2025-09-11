5297a8df8278dde750371f27ad1271bf3af1331f
OpalBSD <nawfalmrouyan@gmail.com>
2025-09-11 12:55:48 +0800

    enable signature_help and inlay_hints using astrolsp.lua

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
@@ -13,8 +13,9 @@ return {
     -- Configuration table of features provided by AstroLSP
     features = {
       codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
+      inlay_hints = true, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
+      signature_help = true, -- enable automatic signature help popup globally on startup
     },
     -- customize lsp formatting options
     formatting = {
