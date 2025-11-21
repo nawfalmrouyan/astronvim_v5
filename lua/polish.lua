80feef4971aafc1347edfdf59e6b0251776deb85
OpalBSD <nawfalmrouyan@gmail.com>
2025-11-21 18:05:11 +0800

    remove comments

--- lua/polish.lua
+++ lua/polish.lua
@@ -88,16 +88,3 @@ do
     vim.keymap.set({ "n", "x", "o" }, key, function() require("leap").leap(as_ft(args)) end)
   end
 end
-
-------------------------------------------------------------------------
--- [1] Match the modes here for which you don't want to use labels
---     (`:h mode()`, `:h lua-pattern`).
--- [2] For 1-char search, you might want to aim for precision instead of
---     typing comfort, to get as many direct jumps as possible.
--- [3] This helper function makes it easier to set "clever-f"-like
---     functionality (https://github.com/rhysd/clever-f.vim), returning
---     an `opts` table derived from the defaults, where:
---     * the given keys are added to `keys.next_target` and
---       `keys.prev_target`
---     * the forward key is used as the first label in `safe_labels`
---     * the backward (reverse) key is removed from `safe_labels`
