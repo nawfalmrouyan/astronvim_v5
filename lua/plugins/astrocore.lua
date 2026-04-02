f4f69ef62e79efbc2e91d3ceffa1b8ae53d9bd0e
OpalBSD <nawfalmrouyan@gmail.com>
2026-04-02 13:49:21 +0800

    formatting

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -35,12 +35,12 @@ return {
     },
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
+      large_buf = { size = 1024 * 256, lines = 10000 },              -- set global limits for large files for disabling features like treesitter
+      autopairs = true,                                              -- enable autopairs at start
+      cmp = true,                                                    -- enable completion at start
       diagnostics = { virtual_text = false, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      highlighturl = true,                                           -- highlight URLs at start
+      notifications = true,                                          -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
@@ -49,11 +49,11 @@ return {
     },
     -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
+      opt = {                  -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+        number = true,         -- sets vim.opt.number
+        spell = false,         -- sets vim.opt.spell
+        signcolumn = "yes",    -- sets vim.opt.signcolumn to yes
         ignorecase = false,
         smartcase = false,
         list = true,
