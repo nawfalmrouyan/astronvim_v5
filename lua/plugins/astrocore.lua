a32d839b64db5a191a2d3e308fbb23a55f904eac
OpalBSD <nawfalmrouyan@gmail.com>
2026-04-08 10:01:17 +0800

    unify astrocore and astrocore_treesitter config files. Remove astrocore_treesitter file.

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -3,6 +3,20 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
+    treesitter = {
+      ensure_installed = { "vim", "lua" },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+      },
+      textobjects = {
+        select = {
+          select_textobject = {
+            ["ak"] = { query = "@block.outer", desc = "around block" },
+          },
+        },
+      },
+    },
     autocmds = {
       set_number = {
         {
