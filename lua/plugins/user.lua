65a3f51b4264fd138bf2733089cf7934cedb34ec
OpalBSD <nawfalmrouyan@gmail.com>
2026-04-09 10:13:57 +0800

    remove stale configs in user.lua. Disable smart-splits and none-ls plugins.

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,34 +1,12 @@
 ---@type LazySpec
 return {
-  -- {
-  --   "saghen/blink.cmp",
-  --   opts = {
-  --     keymap = {
-  --       preset = "default",
-  --       ["<Tab>"] = { "select_next", "fallback" },
-  --       ["<S-Tab>"] = { "select_prev", "fallback" },
-  --       ["<CR>"] = { "accept", "fallback" },
-  --       ["<C-e>"] = { "cancel", "fallback" },
-  --     },
-  --   },
-  -- },
-
   "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function() require("lsp_signature").setup() end,
-  -- },
-
   "max397574/better-escape.nvim",
-  -- "dstein64/vim-startuptime",
-  -- { "tpope/vim-fugitive", cmd = "G" },
 
-  { "none-ls",           optional = true,  enabled = true },
+  { "none-ls",           optional = false, enabled = false },
   { "aerial",            optional = false, enabled = false },
   { "neo-tree.nvim",     optional = false, enabled = false },
-  { "smart-splits.nvim", optional = true,  enabled = false },
-
+  { "smart-splits.nvim", optional = false, enabled = false },
   {
     "folke/persistence.nvim",
     event = "BufReadPre", -- this will only start session saving when an actual file was opened
