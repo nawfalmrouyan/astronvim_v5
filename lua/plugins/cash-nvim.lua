168c6a3a93efcc0187a31a3353c50f8cef865360
OpalBSD <nawfalmrouyan@gmail.com>
2026-09-03 15:59:49 +0800

    ass cash.nvim plugin (custom registers).

---
+++ lua/plugins/cash-nvim.lua
@@ -0,0 +1,8 @@
+return {
+    'nullromo/cash.nvim',
+    opts = {}, -- specify options here
+    config = function(_, opts)
+        local cash = require('cash')
+        cash.setup(opts)
+    end,
+}
